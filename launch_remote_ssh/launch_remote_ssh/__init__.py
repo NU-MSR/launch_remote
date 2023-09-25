2b40d43741cf8f1f84b169f00e56a12c5b87aa1b
Nick Morales <ngmorales97@gmail.com>
2023-09-25 17:01:15 -0500

    Added general copy install space function

--- launch_remote_ssh/launch_remote_ssh/__init__.py
+++ launch_remote_ssh/launch_remote_ssh/__init__.py
@@ -2,12 +2,13 @@ from .replace_text_substitution import ReplaceTextSubstitution
 from .execute_process_remote import ExecuteProcessRemote
 from .node_remote import NodeRemote
 from .launch_remote import LaunchRemote
-from .copy_install import copy_single_package_install
+from .copy_install import copy_single_package_install, copy_install_space
 
 __all__ = [
     'ReplaceTextSubstitution',
     'ExecuteProcessRemote',
     'NodeRemote',
     'LaunchRemote',
-    'copy_single_package_install'
+    'copy_install_space',
+    'copy_single_package_install',
 ]
