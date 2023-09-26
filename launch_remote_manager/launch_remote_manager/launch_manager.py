5fd40b2ab8cb18e8f9631c48fea16cb6c8686bcd
Nick Morales <ngmorales97@gmail.com>
2023-09-26 12:41:03 -0500

    Draft of running launch files as subprocesses

--- launch_remote_manager/launch_remote_manager/launch_manager.py
+++ launch_remote_manager/launch_remote_manager/launch_manager.py
@@ -2,10 +2,11 @@
 
 import os
 from platform import node as get_host_name
+from subprocess import Popen
 
 import rclpy
-from ros2pkg.api import get_prefix_path
 from rclpy.node import Node
+from ros2pkg.api import get_prefix_path
 from launch_remote_interfaces.msg import LaunchFile as LaunchFileMsg
 from launch_remote_interfaces.srv import Launch as LaunchSrv
 
@@ -14,10 +15,13 @@ UNDERLAY_INSTALL_DIR = get_prefix_path('rclpy')
 
 LAUNCH_CMD = 'ros2 launch ${package} ${launch_file}'
 
+SHELL_EXECUTABLE = ''
+
 class LaunchFile:
     def __init__(self, msg : LaunchFileMsg):
         self._msg = msg
         self._compose_cmd()
+        self._process = None
 
     def _compose_cmd(self):
         # Source command
@@ -34,10 +38,6 @@ class LaunchFile:
 
                 self._source_cmd += SOURCE_CMD.replace('${install_dir}', install_dir)
 
-        # TODO(ngmor) remove
-        print(SOURCE_CMD)
-        print(self._source_cmd)
-
         # Launch command
         self._launch_cmd = \
             LAUNCH_CMD.replace(
@@ -51,32 +51,75 @@ class LaunchFile:
         for arg in self._msg.arguments:
             self._launch_cmd += ' ' + arg.name + ':=' + arg.value
 
-        # TODO(ngmor) remove
-        print(LAUNCH_CMD)
-        print(self._launch_cmd)
-
         # Full command
         self._cmd = self._source_cmd + ' && ' + self._launch_cmd
 
-        # TODO(ngmor) remove
-        print(self._cmd)
+    def cmd(self):
+        return self._cmd
+    
+    def run(self):
+        self._process = Popen(self._cmd, shell=True, executable=SHELL_EXECUTABLE)
+
+    def pid(self):
+        if self._process is None:
+            return None
+        else:
+            return self._process.pid
+
+    def stop(self):
+        # TODO(ngmor)
+        pass
+
 
 
 class LaunchManager(Node):
     def __init__(self):
         super().__init__("launch_manager", namespace=get_host_name())
 
+        # Services
+        self._srv_launch = self.create_service(LaunchSrv, 'launch', self._srv_launch_callback)
+
+        self._launch_files = {}
+
+    def _srv_launch_callback(self, request : LaunchSrv.Request, response : LaunchSrv.Response):
+        launch_file = LaunchFile(request.file)
+
+        response.command = launch_file.cmd()
+
+        self.get_logger().info('Running command:\n' + launch_file.cmd())
+
+        launch_file.run()
+
+        if launch_file.pid() is None:
+            
+            self.get_logger().error('Launch file failed to launch:\n' + launch_file.cmd())
+            # TODO(ngmor) handle error
+
+        else:
+            response.pid = launch_file.pid()
+
+            # Store launch file
+            self._launch_files[launch_file.pid()] = launch_file
+
+            # TODO (ngmor) publishing
+
+        return response
 
 def entry(args=None):
     """Spin the node."""
     # TODO(anyone) this is untested on Windows
     # https://stackoverflow.com/questions/1325581/how-do-i-check-if-im-running-on-windows-in-python
     global SOURCE_CMD
+    global SHELL_EXECUTABLE
     if os.name == 'nt':
         SOURCE_CMD = SOURCE_CMD.replace('${source_file}', '\setup.bat')
+        SHELL_EXECUTABLE = 'cmd.exe'
     else:
         SOURCE_CMD = SOURCE_CMD.replace('${source_file}', '/setup.bash')
+        SHELL_EXECUTABLE = '/bin/bash'
 
+    # TODO(ngmor) consider try/catch
+    # TODO(ngmor) explicitly stop all processes on exit
     rclpy.init(args=args)
     launch_manager_node = LaunchManager()
     rclpy.spin(launch_manager_node)
