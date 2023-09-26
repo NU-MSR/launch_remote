27d9660b6e1184dff9949eab745b8d43d418473e
Nick Morales <ngmorales97@gmail.com>
2023-09-26 17:49:32 -0500

    Handle all parameters to launch client

--- launch_remote_manager/launch/test.launch.py
+++ launch_remote_manager/launch/test.launch.py
@@ -6,10 +6,31 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    # Inputs
     machine = 'galagongm'
-    uuid = uuid4()
     package = 'launch_remote_ssh'
     file = 'test_remotely_launched.launch.py'
+    # install_dirs = [''] # TODO(ngmor) must pass this in if empty
+    install_dirs = ['/home/ngm/classes/final/remote_ws/install']
+    # TODO(ngmor) going to have to process these in the same way as LaunchRemote
+    launch_arguments=[
+        ('param1', '-0.567'),
+        ('param2', 'this is a sentence!'),
+        ('param3', '453'),
+    ]
+
+
+    # Logic
+    uuid = uuid4()
+
+    parameters = {}
+    parameters['package'] = package
+    parameters['file'] = file
+    parameters['install_dirs'] = install_dirs
+
+    for i, argument in enumerate(launch_arguments):
+        parameters[f'argname{i}'] = argument[0]
+        parameters[f'argval{i}'] = argument[1]
 
     return LaunchDescription([
         Node(
@@ -18,9 +39,6 @@ def generate_launch_description():
             output='screen', # TODO(ngmor) allow this to be passed through
             name='launch_client_' + f'{uuid.int:x}',
             namespace='/' + machine,
-            parameters=[{
-                'package' : package,
-                'file' : file,
-            }]
+            parameters=[parameters]
         )
     ])
--- launch_remote_manager/launch_remote_manager/launch_client.py
+++ launch_remote_manager/launch_remote_manager/launch_client.py
@@ -2,6 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from rcl_interfaces.msg import ParameterDescriptor
+from launch_remote_interfaces.msg import LaunchArgument as LaunchArgumentMsg
 
 def get_required_parameter_value(node, name):
     if node.has_parameter(name):
@@ -26,6 +27,39 @@ def entry(args=None):
     # Parameters
     package = get_required_parameter_value(launch_client_node, 'package').string_value
     file = get_required_parameter_value(launch_client_node, 'file').string_value
+    install_dirs = \
+        get_required_parameter_value(launch_client_node, 'install_dirs').string_array_value
+    if '' in install_dirs:
+        install_dirs.pop(install_dirs.index(''))
+    
+    arguments = []
+
+    i = 0
+    while(True):
+        param_name = f'argname{i}'
+        param_val = f'argval{i}'
+        if launch_client_node.has_parameter(param_name):
+            argname = \
+                launch_client_node.get_parameter(param_name).get_parameter_value().string_value
+
+            if not (launch_client_node.has_parameter(param_val)):
+                raise Exception(
+                    'Mismatch in number of provided parameter name/value pairs:\n'
+                    f'{param_name}: {argname} did not have a value provided as {param_val}'
+                )
+            
+            argval = \
+                launch_client_node.get_parameter(param_val).get_parameter_value().string_value
+            
+            msg = LaunchArgumentMsg()
+            msg.name = argname
+            msg.value = argval
+            arguments.append(msg)
+        else:
+            break
+        i += 1
+
+    print(arguments)
 
 if __name__ == '__main__':
     entry()
