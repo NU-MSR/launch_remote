64214c40d213aee9764d4f593d04926d1595f607
Nick Morales <ngmorales97@gmail.com>
2023-09-26 11:48:19 -0500

    Add service for launching, build command for launching

--- launch_remote_manager/launch_remote_manager/launch_manager.py
+++ launch_remote_manager/launch_remote_manager/launch_manager.py
@@ -1,9 +1,18 @@
 #!/usr/bin/env python3
 
-import rclpy
+import os
 from platform import node as get_host_name
+
+import rclpy
+from ros2pkg.api import get_prefix_path
 from rclpy.node import Node
 from launch_remote_interfaces.msg import LaunchFile as LaunchFileMsg
+from launch_remote_interfaces.srv import Launch as LaunchSrv
+
+SOURCE_CMD = 'source ${install_dir}${source_file}'
+UNDERLAY_INSTALL_DIR = get_prefix_path('rclpy')
+
+LAUNCH_CMD = 'ros2 launch ${package} ${launch_file}'
 
 class LaunchFile:
     def __init__(self, msg : LaunchFileMsg):
@@ -11,20 +20,63 @@ class LaunchFile:
         self._compose_cmd()
 
     def _compose_cmd(self):
-        self._cmd = 'ros2 launch ' + self._msg.package + ' ' + self._msg.name
+        # Source command
+        if len(self._msg.install_dirs) == 0:
+            self._source_cmd = SOURCE_CMD.replace('${install_dir}', UNDERLAY_INSTALL_DIR)
+        else:
+            self._source_cmd = ''
+
+            first = False
+
+            for install_dir in self._msg.install_dirs:
+                if not first:
+                    self._source_cmd += ' && '
+
+                self._source_cmd += SOURCE_CMD.replace('${install_dir}', install_dir)
+
+        # TODO(ngmor) remove
+        print(SOURCE_CMD)
+        print(self._source_cmd)
+
+        # Launch command
+        self._launch_cmd = \
+            LAUNCH_CMD.replace(
+                '${package}',
+                self._msg.package
+            ).replace(
+                '${launch_file}',
+                self._msg.name
+            )
 
         for arg in self._msg.arguments:
-            self._cmd += ' ' + arg.name + ':=' + arg.value
+            self._launch_cmd += ' ' + arg.name + ':=' + arg.value
 
-        # TODO(nmorales) remove
+        # TODO(ngmor) remove
+        print(LAUNCH_CMD)
+        print(self._launch_cmd)
+
+        # Full command
+        self._cmd = self._source_cmd + ' && ' + self._launch_cmd
+
+        # TODO(ngmor) remove
         print(self._cmd)
+
 
 class LaunchManager(Node):
     def __init__(self):
         super().__init__("launch_manager", namespace=get_host_name())
 
+
 def entry(args=None):
     """Spin the node."""
+    # TODO(anyone) this is untested on Windows
+    # https://stackoverflow.com/questions/1325581/how-do-i-check-if-im-running-on-windows-in-python
+    global SOURCE_CMD
+    if os.name == 'nt':
+        SOURCE_CMD = SOURCE_CMD.replace('${source_file}', '\setup.bat')
+    else:
+        SOURCE_CMD = SOURCE_CMD.replace('${source_file}', '/setup.bash')
+
     rclpy.init(args=args)
     launch_manager_node = LaunchManager()
     rclpy.spin(launch_manager_node)
