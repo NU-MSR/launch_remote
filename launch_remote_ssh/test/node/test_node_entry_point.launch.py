aa16564a272a5e8fa1ff0abeb39bb5a6be17ad65
Nick Morales <ngmorales97@gmail.com>
2023-10-01 13:30:08 -0500

    Start of XML test node launch file

--- launch_remote_ssh/test/node/test_node_entry_point.launch.py
+++ launch_remote_ssh/test/node/test_node_entry_point.launch.py
@@ -28,8 +28,6 @@
 #
 # Author: Nick Morales
 
-import sys
-
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution, TextSubstitution
 from launch.actions import SetLaunchConfiguration, DeclareLaunchArgument
 from launch_ros.parameter_descriptions import Parameter, ParameterFile
